2c7428632f1e9f6e8fa90eee00381d4b139b675b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-02 16:06:45 +0300

    Hibernate, fetch the data, .get() method, SQL, HQL, Session FETCH

--- src/main/java/h1_annotation/RunnerFetch01.java
+++ src/main/java/h1_annotation/RunnerFetch01.java
@@ -4,6 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.sql.Select;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RunnerFetch01 {
     public static void main(String[] args) {
@@ -24,10 +28,40 @@ public class RunnerFetch01 {
                 3. HQL
          */
 
-        // .get() /////////////////////
-        Student01 student01 = session.get(Student01.class, 1001);
-        System.out.println("student01 = " + student01);
+        // .get() =====================
+//        Student01 student01 = session.get(Student01.class, 1001);
+//        System.out.println("student01 = " + student01);
 
+//        Student01 student02 = session.get(Student01.class, 1002);
+//        System.out.println("student02 = " + student02);
+//        System.out.println(student02.getName());
+
+        // SQL: =======================
+//        String sqlQuery = "SELECT * FROM t_student01";
+//        List<Object[]> resultList = session.createSQLQuery(sqlQuery).getResultList();
+//        for (Object[] objects: resultList) {
+//            System.out.println(Arrays.toString(objects));
+//        }
+
+        // HQL: =======================
+//        String hqlQuery01 = "FROM Student01";   // unlike SQL, we have to use java language
+//        List<Student01> resultList01 = session.createQuery(hqlQuery01, Student01.class).getResultList();
+//        for (Student01 student01: resultList01) {
+//            System.out.println("student01 = " + student01);
+//        }
+
+        // TASK: return students sorted by id in descending order
+//        String hqlQuery02 = "FROM Student01 s ORDER BY s.id DESC";
+//        List<Student01> resultList02 = session.createQuery(hqlQuery02, Student01.class).getResultList();
+//        for (Student01 student01: resultList02) {
+//            System.out.println("student01 = " + student01);
+//        }
+
+        // TASK: return student whose name is "Yunus"
+        String hqlQuery03 = "FROM Student01 WHERE name = 'Yunus'";
+
+        Student01 student04 = (Student01) session.createQuery(hqlQuery03, Student01.class).getSingleResult();
+        System.out.println("student04 = " + student04);
 
         transaction.commit();   // if not committed changes/updates will not be updated in DB
         session.close();
