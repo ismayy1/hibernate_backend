7a9500d796ef104192ce0e60a5454023258f1c48
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-07 15:33:09 +0300

    Hibernate, Student08, Book08 example

--- src/main/java/h8_manyToMany/Book08.java
+++ src/main/java/h8_manyToMany/Book08.java
@@ -3,6 +3,8 @@ package h8_manyToMany;
 import h7_bi_oneToMany.Student07;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table (name = "t_book08")
@@ -13,11 +15,32 @@ public class Book08 {
 
     private String name;
 
-
-    private Student07 student;
+    @ManyToMany
+    private List<Student08> student08List = new ArrayList<>();
 
     public int getId() {
         return id;
     }
-
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Student08> getStudent08List() {
+        return student08List;
+    }
+    public void setStudent08List(List<Student08> student08List) {
+        this.student08List = student08List;
+    }
+    @Override
+    public String toString() {
+        return "Book08{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/h8_manyToMany/Student08.java
+++ src/main/java/h8_manyToMany/Student08.java
@@ -18,7 +18,46 @@ public class Student08 {
 
     private int grade;
 
+    @ManyToMany
+    @JoinTable (
+            name = "student08_book08",
+            joinColumns = {@JoinColumn (name = "std_id")},
+            inverseJoinColumns = {@JoinColumn (name = "book_id")}
+    )
+    private List<Book08> book08List = new ArrayList<>();
 
-    private List<Book07> book07List = new ArrayList<>();
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getGrade() {
+        return grade;
+    }
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    public List<Book08> getBook08List() {
+        return book08List;
+    }
+    public void setBook08List(List<Book08> book08List) {
+        this.book08List = book08List;
+    }
 
+    @Override
+    public String toString() {
+        return "Student08{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", book08List=" + book08List +
+                '}';
+    }
 }
