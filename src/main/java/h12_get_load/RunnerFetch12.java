8af01918909a044af2baa8b0e268de48fca0c30d
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-08 19:16:16 +0300

    Hibernate, Student12, get() vs load()

--- src/main/java/h12_get_load/RunnerFetch12.java
+++ src/main/java/h12_get_load/RunnerFetch12.java
@@ -1,9 +1,19 @@
 package h12_get_load;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class RunnerFetch12 {
     public static void main(String[] args) {
 
+        Configuration con = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student12.class);
 
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
 
     }
 }
