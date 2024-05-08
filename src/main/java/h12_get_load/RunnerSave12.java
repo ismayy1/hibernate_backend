0557f62f1735244fa992b9998b7bad050e164852
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-08 19:14:09 +0300

    Hibernate, Student12, get() vs load()

---
+++ src/main/java/h12_get_load/RunnerSave12.java
@@ -0,0 +1,9 @@
+package h12_get_load;
+
+public class RunnerSave12 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/h12_get_load/Student12.java
@@ -0,0 +1,42 @@
+package h12_get_load;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "t_student12")
+public class Student12 {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int grade;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getGrade() {
+        return grade;
+    }
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    @Override
+    public String toString() {
+        return "Student12{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
